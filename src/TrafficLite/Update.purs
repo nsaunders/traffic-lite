4d61dd92eeb85c1cb0c38d3fa764997db04f0959
Nick Saunders <nick@saunde.rs>
2022-12-24 10:46:29 -0700

    Remove unnecessary constraints from update fn.

--- src/TrafficLite/Update.purs
+++ src/TrafficLite/Update.purs
@@ -2,24 +2,12 @@ module TrafficLite.Update where
 
 import Prelude
 
-import Control.Monad.Error.Class (class MonadThrow)
-import Control.Monad.Reader.Class (class MonadAsk)
-import Effect.Aff.Class (class MonadAff)
-import Node.Path (FilePath)
-import TrafficLite.Data.Error as TrafficLite
 import TrafficLite.Data.Metric (mergeDataSets, splitDataSet, unionByTimestamp)
 import TrafficLite.Effect.RemoteData (class MonadRemoteData, fetchClones, fetchViews)
 import TrafficLite.Effect.Store (class MonadStore)
 import TrafficLite.Effect.Store as Store
 
-update
-  :: forall m r
-   . MonadAff m
-  => MonadAsk { path :: FilePath, repo :: String, token :: String | r } m
-  => MonadRemoteData m
-  => MonadStore m
-  => MonadThrow TrafficLite.Error m
-  => m Unit
+update :: forall m. Bind m => MonadRemoteData m => MonadStore m => m Unit
 update = do
   latestClones <- fetchClones
   latestViews <- fetchViews
