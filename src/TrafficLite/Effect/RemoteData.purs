e09543a781f31673450ac999cc90dc4e92c50463
Nick Saunders <nick@saunde.rs>
2022-12-27 18:44:49 -0700

    Additional error handling

--- src/TrafficLite/Effect/RemoteData.purs
+++ src/TrafficLite/Effect/RemoteData.purs
@@ -10,10 +10,14 @@ import Affjax.Node (defaultRequest, request)
 import Affjax.Node as Affjax
 import Affjax.RequestHeader (RequestHeader(..))
 import Affjax.ResponseFormat as ResponseFormat
+import Affjax.StatusCode (StatusCode(..))
 import Control.Monad.Error.Class (class MonadThrow, throwError)
 import Control.Monad.Reader.Class (class MonadAsk, ask)
 import Data.Argonaut (decodeJson, getField, printJsonDecodeError)
+import Data.Argonaut.Decode.Decoders (decodeJObject)
 import Data.Array (sortWith, takeEnd)
+import Data.Bifoldable (bifold)
+import Data.Bifunctor (bimap)
 import Data.Either (either)
 import Data.MediaType (MediaType(..))
 import Effect.Aff.Class (class MonadAff, liftAff)
@@ -35,24 +39,47 @@ fetchCounts
   => String
   -> m (Array { | TimestampRep + CountRep + () })
 fetchCounts metricType = do
+
   { token, repo } <- ask
+
   let
+
     url = "https://api.github.com/repos/" <> repo <> "/traffic/" <> metricType
+
     headers =
       [ Accept $ MediaType "application/vnd.github+json"
       , RequestHeader "Authorization" ("Bearer " <> token)
       , RequestHeader "X-GitHub-Api-Version" "2022-11-28"
       ]
+
     config =
       defaultRequest
         { url = url, headers = headers, responseFormat = ResponseFormat.json }
-  { body } <-
+
+  { status: StatusCode statusCode, body } <-
     either (throwError <<< FetchError <<< Affjax.printError) pure
       =<< liftAff (request config)
-  either
-    (throwError <<< TypeError <<< printJsonDecodeError)
-    (pure <<< takeEnd 13 <<< sortWith _.timestamp)
-    $ decodeJson =<< flip getField metricType =<< decodeJson body
+
+  case statusCode of
+
+    200 ->
+      either
+        (throwError <<< TypeError <<< printJsonDecodeError)
+        (pure <<< takeEnd 13 <<< sortWith _.timestamp)
+        (decodeJson =<< flip getField metricType =<< decodeJson body)
+
+    unexpectedStatusCode ->
+      let
+        msg =
+          (decodeJObject body >>= flip getField "message")
+            # bimap
+                (\_ -> "with no message.")
+                (\m -> "with message: " <> m)
+            # bifold
+      in
+        throwError
+          $ FetchError
+          $ "Unexpected " <> show unexpectedStatusCode <> " response " <> msg
 
 instance MonadRemoteData UpdateM where
   fetchClones = fetchCounts "clones"
